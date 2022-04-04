d30e8f310e7c77c955cbcee937e2a07463b4b7a1
Gaoming Lin <gaominglins@gmail.com>
2022-04-04 12:51:55 -0400

    p

---
+++ day1/Vars.java
@@ -0,0 +1,10 @@
+public class Vars {
+    public static void main(String[] args) {
+		char my_grade ='C';
+        int my_num = 9999999;
+        float PI = 3.14F;
+        double better_PI = 3.1415;
+        String my_name = "Bob";
+        System.out.println(PI);
+	}
+}
