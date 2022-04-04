51515627796e787acdd2fecb153d36c4b72d0310
Gaoming Lin <gaominglins@gmail.com>
2022-04-04 17:43:23 -0400

    p

--- day1/HelloWorld.java
+++ day1/HelloWorld.java
@@ -1,5 +1,9 @@
-public class HelloWorld {
-    public static void main(String[] args) {
+/* PascalCase is the convention where each first letter is a capital letter. */
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
 		System.out.println("Hello World!");
 	}
-}
+};
--- day1/Vars.java
+++ day1/Vars.java
@@ -1,10 +1,12 @@
-public class Vars {
-    public static void main(String[] args) {
-		char my_grade ='C';
-        int my_num = 9999999;
-        float PI = 3.14F;
-        double better_PI = 3.1415;
-        String my_name = "Bob";
-        System.out.println(PI);
+public class Vars
+{
+	public static void main(String[] args)
+	{
+		char my_grade='C';
+		int my_num=123;
+		long my_long=999999999;
+		float PI=3.14f;
+		double BetterPI=3.14159d;
+		String my_name="Bobothy";
 	}
-}
+};
