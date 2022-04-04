0077a6f9be108f14251a2aa66f3452e5579951b6
Gaoming Lin <gaominglins@gmail.com>
2022-04-04 12:51:49 -0400

    p

---
+++ day1/Conditionals.java
@@ -0,0 +1,16 @@
+public class Conditionals {
+    public static void main(String[] arge) {
+        int a = 10;
+        int b =20;
+
+        if(a>b) {System.out.println("a is bigger");}
+        if (a ==b) {System.out.println("a is equal to B ");}
+        if (a <b) {System.out.println("a is smaller ");}
+
+        int i =1;
+        switch (i){
+
+        }
+
+    }
+}
