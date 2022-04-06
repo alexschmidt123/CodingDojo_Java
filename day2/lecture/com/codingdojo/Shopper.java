188fb9f2427575fb6654c8c04c0853cb7f8d2c58
Gaoming Lin <gaominglins@gmail.com>
2022-04-06 02:38:13 -0400

    p

---
+++ day2/lecture/com/codingdojo/Shopper.java
@@ -0,0 +1,16 @@
+package com.codingdojo;
+
+public class Shopper 
+{
+    public String first_name;
+    public String email;
+
+    public Shopper(){
+    
+    }
+
+    public Shopper(String first_name, String email){
+        this.first_name = first_name;
+        this.email = email;
+    }
+};
