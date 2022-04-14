fa984fc7d190888e1e8a224d3ede8f41edb6fedf
Gaoming Lin <gaominglins@gmail.com>
2022-04-14 14:30:29 -0400

    p

---
+++ day3/assignment/CareSoft/src/com/caresoft/clinicapp/Physician.java
@@ -0,0 +1,22 @@
+package com.caresoft.clinicapp;
+import java.util.ArrayList;
+import java.util.Date;
+public class Physician extends User {
+    
+    // Inside class:    
+    private ArrayList<String> patientNotes;
+	
+    // TO DO: Constructor that takes an ID
+    // TO DO: Implement HIPAACompliantUser!
+	
+    public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+	
+    // TO DO: Setters & Getters
+}
